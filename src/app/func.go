16647687f22cbed81ddc90ea55ea7f091455a324
speauty <speauty@163.com>
2022-11-11 18:34:42 +0800

    调整菜单;

--- src/app/func.go
+++ src/app/func.go
@@ -1,7 +1,6 @@
 package app
 
 import (
-	"fmt"
 	"fyne.io/fyne/v2"
 	"gui.fyne.ab/src/app/page"
 	"gui.fyne.ab/src/core/gui"
@@ -21,32 +20,37 @@ func genMenu() *fyne.MainMenu {
 	mainMenu := fyne.NewMainMenu(
 		fyne.NewMenu(
 			"菜单",
-			fyne.NewMenuItem("设置", func() {}),
+			fyne.NewMenuItem("设置", func() {
+				go LogAppApi().Infoln("点击菜单: 菜单-设置")
+			}),
 		),
 		fyne.NewMenu(
 			"服务",
-			fyne.NewMenuItem("AB压测", func() {
-
+			fyne.NewMenuItem("上门采样", func() {
+				go LogAppApi().Infoln("点击菜单: 服务-上门采样")
 			}),
 		),
 		fyne.NewMenu(
 			"帮助",
 			fyne.NewMenuItem("检测版本", func() {
-				fmt.Println("点击检测版本")
+				go LogAppApi().Infoln("点击菜单: 帮助-检测版本")
 			}),
 			fyne.NewMenuItem("关于应用", func() {
-				fmt.Println("点击关于应用")
+				go LogAppApi().Infoln("点击菜单: 帮助-关于应用")
 			}),
 		),
 		fyne.NewMenu(
 			"页面",
 			fyne.NewMenuItem("测试页面", func() {
+				go LogAppApi().Infoln("点击菜单: 页面-测试页面")
 				MainWindow.SetCurrentPageId(page.ApiTestPage().GetId())
 			}),
 			fyne.NewMenuItem("空白页面", func() {
+				go LogAppApi().Infoln("点击菜单: 页面-空白页面")
 				MainWindow.SetCurrentPageId(page.ApiEmptyPage().GetId())
 			}),
 			fyne.NewMenuItem("错误页面", func() {
+				go LogAppApi().Infoln("点击菜单: 页面-错误页面")
 				MainWindow.SetCurrentPageId(page.ApiErrorPage().GetId())
 			}),
 		),
